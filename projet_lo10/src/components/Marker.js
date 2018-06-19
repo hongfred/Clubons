856d6a6d2de04179406ec4faeca8d6be402a52bb
hongfred <frederic.hong@utt.fr>
2018-06-19 17:10:11 +0200

    ça marche

--- projet_lo10/src/components/Marker.js
+++ projet_lo10/src/components/Marker.js
@@ -6,27 +6,29 @@ class Marker extends Component{
 	render(){
 		const image = this.props.$hover ? "https://thumb.ibb.co/kTybaJ/marqueur_Hover.png" : "https://image.ibb.co/ewGdYT/marqueur.png"
 		const disp = this.props.$hover ? "block" : "none"
-		
+
 		return (
 			<div style={{position: 'absolute',
-			width: 56,
-			height: 56,
-			left: -28,
-			top: -28}}>
+				width: 56,
+				height: 56,
+				left: -28,
+				top: -28}}
+			>
 				<img src={image} alt="marqueur"></img>
-			<div class="infoPopup" style={{
-				display: disp
-			}}>
-			{this.props.MesEvents.map(function(event){
-            return(
-				<p><p class="labelPopup">Nom évènement :</p> {event.name}<br/>
-				<p class="labelPopup">Adresse :</p> {event.address}<br/>
-				<p class="labelPopup">Date :</p> {event.date.substring(0,10)}<br/>
-				<p class="labelPopup">Heure :</p> {event.heure.substring(0,5)}<br/>
-				<p class="labelPopup">Description :</p> {event.description}</p>
-            	);
-          	})}
-			</div>
+				<div class="infoPopup" style={{
+					display: disp
+				}}>
+					{JSON.stringify(this.props.MesEvents[0].name)}
+					{this.props.MesEvents.map(function(event){
+		            return(
+									<p><p class="labelPopup">Nom évènement :</p> {event.name}<br/>
+									<p class="labelPopup">Adresse :</p> {event.address}<br/>
+									<p class="labelPopup">Date :</p> {event.date.substring(0,10)}<br/>
+									<p class="labelPopup">Heure :</p> {event.heure.substring(0,5)}<br/>
+									<p class="labelPopup">Description :</p> {event.description}</p>
+		            );
+		      })}
+				</div>
 			</div>
 		);
 	}
